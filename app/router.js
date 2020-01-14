799235cb2eaad201187fd7724e0a0a26e895f1bd
gx <gaoxiangn@yonyou.com>
2020-01-14 10:27:23 +0800

    添加导航页面

--- app/router.js
+++ app/router.js
@@ -5,8 +5,8 @@ const HomeController = require('./controller/index.js')
 
 // 主入口-》路由分发 -》controller-》调用模板进行渲染，加载 service处理服务逻辑
 module.exports = (app) => {
-  router.get( '/', HomeController.index )
-  router.get( '/:component', HomeController.index )
+  router.get( '/', HomeController.guide )
+  router.get( '/:component', HomeController.docs )
   // 注册路由中间件
   app.use(router.routes())
     .use(router.allowedMethods())
